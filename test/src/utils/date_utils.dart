848fa14bf2875f08128fc43eb35b7a64d05e9b0d
Sebastian <sebastianliando@gmail.com>
2021-06-01 16:58:22 +0800

    Add date parsing for album added date

---
+++ test/src/utils/date_utils.dart
@@ -0,0 +1,29 @@
+import 'package:khinsider_api/src/utils/date_utils.dart';
+import 'package:test/test.dart';
+
+String getKhinsiderAddedDate(String ordinalDate, String month, String year) =>
+    '$month $ordinalDate, $year';
+
+void main() {
+  group('Album Added Date', () {
+    test('Parse month with the same name as the same abbreviation', () {
+      final addedDate = getKhinsiderAddedDate('26th', 'May', '2021');
+
+      final parsed = parseKhinsiderAddedDate(addedDate);
+
+      expect(parsed.day, 26);
+      expect(parsed.month, DateTime.may);
+      expect(parsed.year, 2021);
+    });
+
+    test('Parse month with a different name than the same abbreviation', () {
+      final addedDate = getKhinsiderAddedDate('23rd', 'Apr', '2021');
+
+      final parsed = parseKhinsiderAddedDate(addedDate);
+
+      expect(parsed.day, 23);
+      expect(parsed.month, DateTime.april);
+      expect(parsed.year, 2021);
+    });
+  });
+}
