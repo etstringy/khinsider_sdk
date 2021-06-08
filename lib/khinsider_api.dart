7e01f42fe223f1f98bed270884123c9a032a2913
Sebastian <sebastianliando@gmail.com>
2021-06-08 14:43:49 +0800

    Export public classes

--- lib/khinsider_api.dart
+++ lib/khinsider_api.dart
@@ -1,1 +1,9 @@
 library khinsider_api;
+
+// Client
+export 'package:khinsider_api/src/core/khinsider.dart';
+
+// Data classes
+export 'package:khinsider_api/src/models/album.dart';
+export 'package:khinsider_api/src/models/album_summary.dart';
+export 'package:khinsider_api/src/models/soundtrack.dart';
